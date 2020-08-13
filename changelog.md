02603eeb695167d962cec6ed772dde3b58df0f63
Anthony <sluzquinosa@uni.pe>
2020-08-13 00:32:46 -0500

    v 0.2.0

--- changelog.md
+++ changelog.md
@@ -3,3 +3,7 @@
 ## Version 0.1.0
 
 - Project initialization.
+
+## Version 0.2.0
+
+- The members html is now being injected with JS.
--- public/js/index.js
+++ public/js/index.js
@@ -12,31 +12,93 @@ const ulNavbar = document.getElementById('navList')
 const navbarItems = [].slice.call(document.getElementsByTagName('li'))
 
 const contentInfo = Array.from(document.querySelectorAll('.info'))
+const html = [`
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-3">
+            <img class="photo" src="https://s.gravatar.com/avatar/07a063fd3d89fba1e6905cfda2f2b323?s=80"
+              alt="Anthony's photo">
+          </div>
+          <div class="col-9 name text-primary">Steve Anthony Luzquiños Agama</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-3">
+            <img
+              src="https://lh3.googleusercontent.com/-7GPu592YbTQ/AAAAAAAAAAI/AAAAAAAAAAA/AUrWqv7xLjo/s100-p/photo.jpg"
+              alt="Luis's photo" class="photo fix-photo">
+          </div>
+          <div class="col-9 name text-primary">Luis Angel Flores Huamaní</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-3">
+            <img src="https://www.orce.uni.edu.pe/fotosuni/006020162665B.jpg" alt="Cristopher's photo"
+              class="photo fix-photo">
+          </div>
+          <div class="col-9 name text-primary">Cristopher Sebastián García Pacheco</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-3">
+            <img
+              src="https://scontent.flim23-1.fna.fbcdn.net/v/t31.0-8/23157449_1502793483136936_6527755451930943033_o.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_eui2=AeHKts02AoY2ojY_luUNxRgOF4IZ19QPIVQXghnX1A8hVFa_zEws3-k9ZrQPoeL3s7dHETAxCTx1juZzr5ddhqGb&_nc_ohc=rBtucD7C0EUAX9KEBu2&_nc_ht=scontent.flim23-1.fna&oh=220c8165368025ed3962ef8bdeac9729&oe=5F3B6C0E"
+              alt="Diegos's photo" class="photo fix-photo">
+          </div>
+          <div class="col-9 name text-primary">Diego Andrade Carril</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-6">
+      <div class="container">
+        <div class="row padding">
+          <div class="col-3">
+            <img
+              src="https://scontent.flim23-1.fna.fbcdn.net/v/t1.0-1/p200x200/103761161_2739658976312304_4733472957537814209_o.jpg?_nc_cat=109&_nc_sid=7206a8&_nc_ohc=CEwCN43EC6IAX-YdDFu&_nc_ht=scontent.flim23-1.fna&_nc_tp=6&oh=7bae8af451b8008e4414a880275fe05c&oe=5F3A0659"
+              alt="Luis Sullca's photo" class="photo fix-photo">
+          </div>
+          <div class="col-9 name text-primary">Luis Alfredo Sullca Huaraca</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6"></div>
+  </div>
+</div>
+`, '', '', '', '']
+
+contentInfo[0].innerHTML = html[0]
 
 const navbarClickItem = id => {
   const clickedItem = document.getElementById(id)
+
   navbarItems.forEach((item, index) => {
     item.classList.remove('active')
-    contentInfo[index].classList.remove('visible')
-    contentInfo[index].classList.remove('invisible')
+    if (item.id === id) {
+      contentInfo[index].classList.remove('invisible')
+      contentInfo[index].classList.add('visible')
+      contentInfo[index].innerHTML = html[index]
+    } else {
+      contentInfo[index].classList.remove('visible')
+      contentInfo[index].classList.add('invisible')
+      contentInfo[index].innerHTML = ''
+    }
   })
-
-  switch (id) {
-    case 'members':
-      contentInfo[0].classList.add('visible')
-      break
-    case 'memories':
-      contentInfo[1].classList.add('visible')
-      break
-    case 'net-documentation':
-      contentInfo[2].classList.add('visible')
-      break
-    case 'structure':
-      contentInfo[3].classList.add('visible')
-      break
-    case 'admin':
-      contentInfo[4].classList.add('visible')
-  }
 
   clickedItem.classList.add('active')
 }
